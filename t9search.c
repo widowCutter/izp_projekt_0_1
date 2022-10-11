928a12874c204fb0fddd133d5c206ba01eac05c7
widowCutter <ondrej@pribyl.tech>
2022-10-11 17:32:11 +0200

    implemented parsing arguments 95/100

--- t9search.c
+++ t9search.c
@@ -10,6 +10,15 @@ struct numArray{
     int allocatedLen;
     int trueLenght;
 };
+struct contact {
+    struct string text;
+    int number;
+};
+struct numberList{
+    struct contact *contList;
+    int alocLenght;
+    int trueLenght;
+};
 //Checks if parameters are within expected patterns
 //If not returns with specified error code
 int sanitizeInput(int argc, char *argv[])
@@ -17,7 +26,7 @@ int sanitizeInput(int argc, char *argv[])
     if (argc < 2) {return 10;}
     for (int i = 1; argc > i; i++) {
         for (int o = 0; (argv[i][o]) != '\0'; o++) {
-            printf("%c", argv[i][o]);
+            if (argv[i][o] < 48 || argv[i][o] > 57) {return 10;}
         }
         printf("\n");
     }
@@ -31,7 +40,44 @@ struct numArray getParameters(int argc, char* argv[])
     pureArray = malloc(sizeof(int) * 10);
     struct numArray array;
     array.numbers = pureArray;
+    array.trueLenght = 0;
+    for (int i = 1; argc > i; i++) {
+        sscanf(argv[i], "%d", &array.numbers[i - 1]);
+        array.trueLenght++;
+    }
     return array;
+}
+
+void printArguments(struct numArray array)
+{
+    printf("Lenght of array is :%d\n", array.trueLenght);
+    for (int i = 0; array.trueLenght > i; i ++) {
+        printf("Cislo:%d je :[%d]\n", i,array.numbers[i]);
+    }
+    return;
+}
+
+//Parse input txt into struct numberList
+//Format is "%s\n%d"
+//
+
+struct contact parseContact(FILE *fp)
+{
+    struct contact parsedContact;
+    return parsedContact;
+}
+
+struct numberList parseTable(char *path)
+{
+    struct numberList numberList;
+    numberList.contList = malloc(sizeof(contact) * 10);
+    numberList.alocLenght = 10;
+    numberList.trueLenght = 0;
+    for (int i = 0; ; i++) {
+    }
+    FILE *fp;
+    fp = fopen(path, "r");
+    return numberList;
 }
 
 //Program will run with one argument: {
@@ -52,6 +98,8 @@ int main (int argc, char *argv[])
         default:
             return -99;
     }
-    
+    struct numArray array = getParameters(argc, argv);
+    printArguments(array);
+    struct numberList numList = parseTable("./inTable.txt");
     return 0;
 }
